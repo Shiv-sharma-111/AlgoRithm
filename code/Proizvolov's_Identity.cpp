0c2a0c51dedd0ebe7041de93c2736ce4c8dde970
Shiv Sharma <36760040+Shiv-sharma-111@users.noreply.github.com>
2019-08-13 18:57:59 +0530

    Geeks For Geek

---
+++ code/Proizvolov's_Identity.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int proizvolov(int a[],int b[], int n)
+{
+	return n*n;
+}
+//Driver Function
+int main()
+{
+	int a[] = {1,5,6,8,10}, b[]={9,7,4,3,2};
+	int n=sizeof(a)/sizeof(a[0]);
+	cout<<proizvolov(a,b,n);
+	return 0;
+}
